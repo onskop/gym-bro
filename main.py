b393a20bb401ab012ec1fe836119ca2600392242
onskop <on.skop@gmail.com>
2023-09-14 15:57:30 +0200

    .

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 # read file content
-with open(os.path.join(os.path.dirname(__file__), 'novejfile.txt'), 'r') as f:
+with open('novejfile.txt', 'r') as f:
     data = f.read()
 
 
